908aa61d2b0d6b463eb3f3928f2b378a4645aa0f
brandonh0515 <107810726+brandonh0515@users.noreply.github.com>
2022-09-20 06:56:35 -0400

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "private_cidr" {
+  type = string
+}

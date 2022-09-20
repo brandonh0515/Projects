cfbf3b411178c665ee863102460926122540b73a
brandonh0515 <107810726+brandonh0515@users.noreply.github.com>
2022-09-20 06:57:10 -0400

    Create providers.tf

---
+++ Terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = "***********ZC2O"
+  secret_key = "*********************APKO"
+}
